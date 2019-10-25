c3a433dd45c1de384275de1a1d9631abc9d66c27
Miquel Guspí Tricas <miquel.guspi@enti.cat>
2019-10-25 10:06:38 +0200

    coroutines

---
+++ Assets/newclass.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NomUnityClass
+{
+    bool running = false;
+
+    TextMeshProUGUI textToChange;
+    public NomUnityClass(ref TextMeshProUGUI txt)
+    {
+        textToChange = txt;
+    }
+
+    public IEnumerator ChangeText()
+    {
+        if(running)
+            yield break;        //parar corutina
+
+        running = true;         
+
+        int maxChar = textToChange.text.Length;
+        textToChange.maxVisibleCharacters = 0;
+        int characters = 0;
+
+        
+
+        while(characters<maxChar)
+        {
+            characters++;
+            textToChange.maxVisibleCharacters = characters;
+            yield return new WaitForSeconds(0.1f);
+        }
+        running = false;
+    }
+
+}
---
+++ Assets/text_behaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class text_behaviour : MonoBehaviour
+{
+    [SerializeField]        //permet visualitzar variables privades a l'inspector
+    private TextMeshProUGUI text;
+    int a = 10;
+    float b = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.GetInt("vida", -1); //si no troba vida, retorna -1
+        PlayerPrefs.SetInt("vida", 10); //guarda les variables fora de unity
+        PlayerPrefs.DeleteKey("vida");
+
+        NomUnityClass hey = new NomUnityClass(ref text);
+        StartCoroutine(hey.ChangeText());
+        
+    }
+
+  
+    /*
+    IEnumerator WriteText()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log(text.text);
+            yield return new WaitForEndOfFrame();
+        }
+        yield return new WaitForSeconds(1f);
+    }                                           //posar operacions complicades en coroutines pot reduir els FPS
+    */
+
+
+}
